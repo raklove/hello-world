05c668a9b2cd27e920cab5b97c4824a674a072dd
raklove <raklove@msn.com>
2016-03-03 08:13:46 +0400

    Update the tags

--- flaskapp.py
+++ flaskapp.py
@@ -31,15 +31,3 @@ app.debug = True
 if __name__ == "__main__":
         app.run("0.0.0.0")
 
-# this is the new comment
-
-
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run()
